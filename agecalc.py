7d7570034081747692412cc576a39328bb86ebef
DANNY SAMANGOMA <dannysamangoma9@gmail.com>
2025-09-19 12:33:37 +0200

    Add files via upload

---
+++ agecalc.py
@@ -0,0 +1,5 @@
+name = input('enter your name: ')
+birth_year = int(input('enter birth year: '))
+current_year = 2025
+age = current_year-birth_year
+print ('hello',name+'!you are',age,'years old.')
